6d3ed03d675dce2589b461f3ed1856e1b833fd7a
3mr18 <hindflower352@gmail.com>
2026-05-20 12:12:41 +0300

    feat: add HMS views and module manifest

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "HMS",
+    "summary": "Hospital management system",
+    "category": "Healthcare",
+    "author": "Amr",
+    "depends": ["base"],
+    "data": [
+        "views/hms_patient_views.xml",
+        "views/hms_department_views.xml",
+        "views/hms_doctor_views.xml",
+        "views/res_partner_views.xml"
+    ],
+    "application": True,
+    "installable": True,
+    "license": "LGPL-3"
+}
